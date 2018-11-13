73d642e537e036e68ecff52847b10b06286f59a7
cyuzuzo <yves.cyuzuzo@wiredin.rw>
2018-11-13 08:45:46 +0200

    Add table view with static dummy data and search bar

--- frontend_challenge/src/App.js
+++ frontend_challenge/src/App.js
@@ -1,25 +1,73 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'react-bootstrap-table/dist/react-bootstrap-table.min.css';
+import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
+  
 
 class App extends Component {
+  state = {
+      isLoading: true,
+      launches: [],
+      error: null
+    }
+
+  //function fetch data
+  fetchAllLauches() {
+    let launchArr = [
+      {
+        flight_number: 77,
+        mission_name: 'NY',
+        rocket_name: 'NY2323'
+
+      },
+      {
+        flight_number: 72,
+        mission_name: 'KGL',
+        rocket_name: '232323KGL'
+
+      },
+      {
+        flight_number: 12,
+        mission_name: 'UG',
+        rocket_name: 'dsadUG21'
+
+      }
+    ]
+
+    this.setState({
+      launches: launchArr,
+      isLoading: false,
+    })
+    
+  }
+
+  componentDidMount() {
+    this.fetchAllLauches();
+  }
+
   render() {
+    const { isLoading, launches, error } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <h3>Frontend React Coding Challenge - Yves CYUZUZO</h3>
+
+        <h2>SpaceX 20 Latest launches</h2>
+
+        {error ? <p>{error.message}</p> : null}
+
+        <div className="container">
+          {!isLoading ? (
+
+            <BootstrapTable data={launches} striped={true} hover={true} search searchPlaceholder={"Search mission name"}>
+                  <TableHeaderColumn dataField="flight_number" isKey={true} dataAlign="center" dataSort={true} searchable={false}>Flight number</TableHeaderColumn>
+                  <TableHeaderColumn dataField="mission_name"  dataSort={true}>mission name</TableHeaderColumn>
+                  <TableHeaderColumn dataField= "rocket_name" searchable={false}>rocket name</TableHeaderColumn>
+              </BootstrapTable>
+            ): (
+              <h4>Loading...</h4>
+            )
+          }
+        </div>
       </div>
     );
   }
